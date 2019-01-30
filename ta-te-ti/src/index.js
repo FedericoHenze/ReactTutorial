a8b16206613e6324be74b8cb9807b05166a135a5
Henze Federico <federico.henze@gmail.com>
2019-01-30 23:00:11 +0100

    Adding missing move index to the button label

--- ta-te-ti/src/index.js
+++ ta-te-ti/src/index.js
@@ -86,9 +86,9 @@ class Game extends React.Component {
         const winner = calculateWinner(current.squares)
 
         const moves = history.map((step, move) => {
-            const desc = move ? 'Go to move #' : 'Go to game start';
+            const desc = move ? `Go to move #${move}` : 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
